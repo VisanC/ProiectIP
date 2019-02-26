3e64dbcff92598ca82041ae19b3bb2f9870d7c0c
Florix1 <hategan.floringeorge@yahoo.com>
2019-02-26 15:54:09 +0200

    Server prelude

---
+++ Server/Server/MessageToReceive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class MessageToReceive
+    {
+        byte type;
+        String[] args;
+
+        public MessageToReceive(Byte[] msg)
+        {
+            if (msg.Length > 0)
+                type = msg[0];
+            else
+                Console.WriteLine("Dimensiune 0!");
+
+            msg = msg.Skip(1).ToArray(); //skip type
+            String temp = Encoding.ASCII.GetString(msg); //make String
+            args = temp.Split('~'); //get arguments
+            foreach (String s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,53 @@ namespace Server
 {
     class Program
     {
+        public static Byte[] aux;
+        public static String ReceiveMessage(NetworkStream stream)
+        {
+            Byte[] dir = new byte[1024];
+            int size = stream.Read(dir, 0, dir.Length);
+
+            Program.aux = new byte[size];
+            Array.Copy(dir, aux, size);
+            String msg = Encoding.ASCII.GetString(aux);
+
+            return msg;
+        }
+
         static void Main(string[] args)
         {
+            /*
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+            try
+            {
+                server.Start();
+                Console.WriteLine("Waiting for Edita!....");
+                while (true)   //we wait for a connection
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+                    byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
+                    hello = Encoding.Default.GetBytes("0");  //conversion string => byte array
+                    Console.WriteLine("Connected");
+                    Console.WriteLine(ReceiveMessage(ns));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write("Exceptie " + e.GetType().ToString() );
+            }
+            */
+
+            String s = "0Penis~Pula~Edita";
+
+            MessageToReceive m = new MessageToReceive(Encoding.ASCII.GetBytes(s));
+            while (true)
+            {
+                ;
+
+            }
         }
     }
+
 }
