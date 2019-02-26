9ae90c6119b903bdf1da39394367f6bcc3946617
VisanC <tudorcosmin.visan@gmail.com>
2019-02-26 13:44:51 +0200

    Added Edward's client work

---
+++ Client/MessageToSend.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client
+{
+    class MessageToSend {
+        byte[] buffer = Encoding.ASCII.GetBytes(input);
+
+        public MessageToSend(byte type, String[] args)
+        {
+            int len = 1, nr = 0;
+            foreach ( String s in args)
+            {
+                len += s.Length;
+                nr++;
+            }
+            buffer = new byte[len + nr];
+            buffer[0] = type;
+            foreach (byte b in buffer)
+            {
+                if ( b == type )
+                {
+                    continue;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int port = 9999;
+                TcpClient client = new TcpClient();
+                client.Connect(System.Net.IPAddress.Parse("192.168.1.169"), port);
+                String message = "Salut";
+                // Get a client stream for reading and writing.
+                NetworkStream stream = client.GetStream();
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
