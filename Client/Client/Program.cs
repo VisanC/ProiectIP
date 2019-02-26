48804eb17ec321af5fa60107d272803fbd0ad969
VisanC <tudorcosmin.visan@gmail.com>
2019-02-26 13:53:23 +0200

    Facut curat

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Client
 {
@@ -10,6 +8,19 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                int port = 9999;
+                TcpClient client = new TcpClient();
+                client.Connect(System.Net.IPAddress.Parse("192.168.1.169"), port);
+                String message = "Salut";
+                // Get a client stream for reading and writing.
+                NetworkStream stream = client.GetStream();
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+            }
         }
     }
 }
